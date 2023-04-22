cbd79d9fe21fab085a5bc4be3fea8abcd77e832b
changach-3000 <sharon.changach@student.moringaschool.com>
2023-04-23 00:55:44 +0300

    Added new comment

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -8,6 +8,7 @@ function BlogPost() {
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
       <BlogContent articleText="This is a second BlogText" />
       <Comment commentText="I agree with this statement. - Angela Merkel" />
+      <Comment commentText = "naah, I've seen better!"/>
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
     </div>
