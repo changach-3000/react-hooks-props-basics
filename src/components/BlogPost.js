52edec7038bde85b176730009fbe89815df45a5a
changach-3000 <sharon.changach@student.moringaschool.com>
2023-04-23 00:58:20 +0300

    Added more props to BlogContent

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -6,7 +6,8 @@ function BlogPost() {
   return (
     <div id="blog-post">
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
-      <BlogContent articleText="This is a second BlogText" />
+      <BlogContent articleText="This is a second BlogText" 
+      readers={45}/>
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText = "naah, I've seen better!"/>
       <Comment commentText="A universal truth. - Noam Chomsky" />
